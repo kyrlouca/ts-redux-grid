d1b81fff9ef3b7b107795d0ecf2a440d7385e3cf
kyrlouca <kyrlouca@gmail.com>
2020-01-08 09:55:31 +0200

    dispatch works

--- src/store/use-redux-actions.tsx
+++ src/store/use-redux-actions.tsx
@@ -1,15 +1,29 @@
 import { useDispatch } from 'react-redux';
+import axios from 'axios';
 import { TStoreType, Tcustomer } from '../store/redux-reducer';
-
-const customers: Tcustomer[] = [
-  { firstName: 'ccFirst' },
-  { firstName: 'ccSecond' }
-];
 
 export default function useReduxActions() {
   const dispatch = useDispatch();
-  const getCust = () => {
-    dispatch({ type: 'GET_CUSTOMERS', payload: customers });
+  const getCust = async () => {
+    try {
+      const resp = await axios.get(
+        `https://f8h018dhb4.execute-api.eu-west-2.amazonaws.com/Prod/orders`,
+        {
+          params: {
+            custId: 'CUST#101#'
+          }
+        }
+      );
+      const orders = resp.data.Items.filter(
+        (item: any) => item.TABLE_NAME === 'ORDER'
+      ).map((item: any) => {
+        return { firstName: item.orderValue };
+      });
+      console.log(orders);
+      dispatch({ type: 'GET_CUSTOMERS', payload: orders });
+    } catch (error) {
+      console.log(error);
+    }
   };
   return {
     getC: getCust
